2ca2b1e0711bff71a91a362499ec1ed28c1258f7
Sara Pena <pena.e.sara@gmail.com>
2019-10-09 06:17:50 -0500

    Create 10.09.19_count_characters_in_your_string.py

---
+++ 10.09.19_count_characters_in_your_string.py
@@ -0,0 +1,13 @@
+"""The main idea is to count all the occuring characters(UTF-8) in string.
+
+If you have string like this aba then the result should be { 'a': 2, 'b': 1 }
+
+What if the string is empty ? 
+
+Then the result should be empty object literal { }"""
+
+def count(string):
+    c = {}
+    for l in string:
+        c[l] = string.count(l)
+    return c
